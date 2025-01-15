3f3b58e86f42d14d60b57f082ad046980e76eb64
Jessica Plemmons <jessica.plemmons@hotmail.com>
2025-01-14 20:39:35 -0500

    experimented some more also, commented everything out

--- js/index.js
+++ js/index.js
@@ -2,10 +2,32 @@
 // that are not declared
 //"use strict"
 
+/*
+console.log(1 + 1);         // Output: 2
+console.log(1 - 1);          // Output: 0
+console.log(1 + "1");        // Output: 11
+                             // What? Why? Search for the answer
 
+console.log(3 - 4 * 5)       // Output: -17
+console.log((3 - 4) * 5);    // Output: -5
+                             // Remember order of precendence
 
+console.log(6 / 5);          // Output: 1.2
+console.log(6 % 5);          // Output: 1
+                             // Modulus return the remainder. This operator is extremely useful.
 
+console.log(1 == 1);         // Output: true
+console.log(1 == "1");       // Output: true
+console.log(1 === "1");      // Output: false
 
-//console.log(1 || 2);         // Output: 1
+console.log(1 || 2);         // Output: 1
+console.log(2 || 1);         // Output: 2
+console.log(1 && 2);         // Output: false
+console.log(1 && 1);         // Output: true
+*/
 
+// console.log(1 + 1);         // Output: 2
+// console.log(1 - 1);          // Output: 0
+//console.log(1 + "1");        // Output: 11
 
+//console.log(1 && 2);         // Output: false unsure why output is 2?
